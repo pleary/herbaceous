df1b2548ede1bb21d41b226b69fce6746d269372
Patrick Leary <pleary@mbl.edu>
2013-04-05 21:26:50 -0400

    Added styl module for CSS and jade module for HTML; using express routes; ignore node_modules

---
+++ routes/index.js
@@ -0,0 +1,3 @@
+exports.index = function(req, res){
+  res.render('feed', { title: 'herb.aceo.us' });
+};
--- web.js
+++ web.js
@@ -1,40 +1,61 @@
 var express = require('express');
+var http = require('http');
+var path = require('path');
+var stylus = require('stylus');
+var routes = require('./routes');
+var site = require('./routes/index');
 var app = express();
-var server = require('http').createServer(app);
+
+/*  configure node, express  */
+app.configure(function(){
+  app.set('port', process.env.PORT || 5000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(stylus.middleware({
+    src: __dirname + '/public',
+    dest: __dirname + '/public'
+  }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+/*  configure express routes  */
+app.get('/', site.index);
+
+/*  start node  */
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+
+
+
+/*  configure socket.io  */
 var io = require('socket.io').listen(server);
-var fs = require('fs');
-
-// GET /javascripts/jquery.js
-// GET /style.css
-// GET /favicon.ico
-app.use(express.static(__dirname + '/public'));
-
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+io.configure(function () {
+  // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+  io.set("log level", 1);
 });
 
-var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
-server.listen(port, function() {
-  console.log("Express server listening on port %d", server.address().port);
-});
-
+/*  socket.io events  */
 var openConnections = 0;
 io.sockets.on('connection', function (socket) {
   openConnections++;
   io.sockets.emit('update_user_count', openConnections);
-  // socket.on('connect', function () {
-  //   openConnections++;
-  //   io.sockets.emit('update_user_count', openConnections);
-  // });
+
   socket.on('disconnect', function () {
     openConnections--;
     io.sockets.emit('update_user_count', openConnections);
   });
+
   socket.on('lets_all_look_at', function(taxon_data) {
-    fs.appendFile('search_log.txt', JSON.stringify(taxon_data)+"\n", function (err) {
-      if (err) throw err;
-    });
     socket.broadcast.emit('set_image', taxon_data);
   });
 });
